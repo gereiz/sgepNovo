eab16d6b25cea35b02b52ff241edd4296ca43ec3
georgiereiss <georgie.reis@farmaciaindiana.com.br>
2023-07-21 11:05:02 -0300

    Atualização Layout Pagina de Painéis

--- resources/js/Pages/Paineis/ListaPaineis.vue
+++ resources/js/Pages/Paineis/ListaPaineis.vue
@@ -11,7 +11,7 @@ const pesqPainel = ref('');
 const paineisFiltrados = computed(() => {
     let paineisFiltrados = Object.values(props.paineis).filter((painel) => {
         return (
-            String(painel.ponto_referencia).toLowerCase().indexOf(pesqPainel.value.toLowerCase()) > -1
+            String(painel.bairro.nome).toLowerCase().indexOf(pesqPainel.value.toLowerCase()) > -1
         );
     })
 
@@ -19,7 +19,10 @@ const paineisFiltrados = computed(() => {
 });
 
 function getImage(i) {
-    // var image = '../../../../storage/app/public/'+ i
+    // Devenvolvimento
+    // var image = 'http://[::1]:5173/storage/app/public/'+ i 
+
+    // Produção
     var image = '/storage/app/public/'+ i
 
     return image
@@ -32,13 +35,13 @@ function getImage(i) {
     <Head title="Painéis" />
 
     <AuthenticatedLayout>
-        <div class="w-full h-screen pt-24 pb-32 mx-2 md:mx-4">
+        <div class="w-full h-screen pt-24 pb-32 mx-2 sm:mx-4">
             
             <!-- Cabeçalho e barra de Pesquisa -->
-            <div class="w-full h-14 flex mb-2">
+            <div class="w-full h-14 flex mb-2 pe-3 sm:pe-0">
                 <div class="w-2/12 h-14 flex items-center">
-                    <h1 class="text-xl md:text-4xl font-bold">Painéis</h1>
-                    <h1 class="text-lg md:text-2xl text-red-400 font-bold ml-2 md:ml-4">{{ paineis.length }}</h1>
+                    <h1 class="text-xl sm:text-4xl font-bold">Painéis</h1>
+                    <h1 class="text-lg sm:text-2xl text-red-400 font-bold ml-2 sm:ml-4">{{ paineis.length }}</h1>
                 </div>
                 
                 <div class="w-10/12 flex justify-end">
@@ -46,32 +49,43 @@ function getImage(i) {
                 </div>
                 
             </div>
-            <div class="w-full md:w-4/12">
+
+            <!-- Barra de Pesquisa -->
+            <div class="w-full sm:w-4/12">
                 <input v-model="pesqPainel" placeholder="Pesquisar Painel" class="w-full h-10 input input-bordered rounded-none mb-4" type="text" name="pesquisar" id="pesquisar">
             </div>
 
+            <!-- Card Principal -->
             <div class="card w-full h-full bg-base-100 shadow-xl overflow-auto rounded-md">
                 <div class="card-body">
-                    <div class="w-full flex flex-col flex-wrap md:flex-row justify-center">
-                   
-                        <div v-for="(pain, index) in paineisFiltrados" :key="index" class="card w-full md:w-5/12 bg-base-100 border-2 rounded-md shadow-xl mt-4 md:mr-4">
-                            <label for="modal-cliente" @click="setClienteData(pain.id, (pain.logradouro ? pain.logradouro : pain.ponto_referencia))">
+                    <div class="w-full flex flex-col flex-wrap sm:flex-row justify-center">
+                        
+                        <!-- Cards dos Paineis -->
+                        <div v-for="(pain, index) in paineisFiltrados" :key="index" class="card w-full sm:w-5/12 bg-base-100 border-2 rounded-md shadow-xl mt-4 sm:mr-4">
+                            <label for="modal-cliente">
                                 <div class="card-body">
-                                    <div class="w-full flex justify-between flex-wrap mb-4">
-                                        <div class="w-full flex justify-between">
-                                            <h2 v-if="pain.logradouro === null" class="text-xs md:card-title">Painel.: {{pain.ponto_referencia}}</h2>
-                                            <h2 v-else class="text-xs md:card-title">Painel.: {{pain.identificacao}}</h2>
-                                            <h2 class="text-xs md:text-base font-bold text-zinc-400">ID: {{pain.id}}</h2>
+                                   <div class="w-full flex flex-col items-center">
+                                        <div class="w-full mb-4 -ml-4">
+                                            <img class="img-painel" :src="getImage(pain.image_url)" alt="Bairro">
                                         </div>
-                                    </div>
 
-                                    <div class="w-full flex justify-center md:justify-start mb-4 md:mb-0">
-                                            <img class="w-20 md:w-32" :src="getImage(pain.image_url)" alt="Bairro">
-                                    </div>
 
-                                    <!-- <div class="w-full card-actions justify-center md:justify-end ">
-                                        <label for="modal-cliente" @click="setBairroData(pain.id, pain.nome_fantasia)" class="w-full md:w-28 botao-modal">Ações</label>
-                                    </div> -->
+                                        <div class="w-full sm:w-11/12 flex justify-center flex-wrap mb-4">
+                                            <div class="w-full flex justify-between sm:justify-between">
+                                                <h2 class="text-xs sm:card-title">Bairro: {{pain.bairro.nome}}</h2>
+                                                <h2 class="text-xs sm:card-title text-red-500">Identificação.: {{pain.identificacao}}</h2>
+                                            </div>
+                                        </div>
+
+                                        <!-- Botões -->
+                                        <div class="w-full sm:w-11/12 flex justify-center flex-wrap space-x-2 mb-4">
+                                            <button class="botao bg-sky-700">Detalhes</button>
+                                            <button class="botao bg-yellow-700">Editar</button>
+                                            <button class="botao bg-red-700">Excluir</button>
+                                        </div>
+
+                                   </div>
+                                
                                 </div>
                             </label>
                         </div>
