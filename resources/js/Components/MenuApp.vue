c97fbe8f63b93fc3ceba6e4a6efec973772be383
georgiereiss <georgie.reis@farmaciaindiana.com.br>
2023-10-24 17:50:44 -0300

    Criado Relatório de Colagem
    - Criado Relatório de Colagem

--- resources/js/Components/MenuApp.vue
+++ resources/js/Components/MenuApp.vue
@@ -74,13 +74,14 @@
     <!-- Relatórios -->
     <div class="w-full flex">
         <div class="w-2/12 md:ml-2.5 text-center">
-            <i class="fa-regular fa-file text-white text-xl"></i> 
+            <i class="fa-regular fa-file text-white text-xl"></i>
         </div> 
         <div class="w-10/12 flex">
             <div class="group">
             <p  class="text-white mt-1">Relatórios</p>
                 <ul  class="hidden group-hover:block w-52 text-white ml-1 mt-2 space-y-2">
-                    <li class="hover:text-slate-400"><a href="https://old.sgepequipe.com/relColagem" target="_blank">Relatório de Colagem</a></li>
+                    <li class="hover:text-slate-400"><a href="/RelColagem">Relatório de Colagem</a></li>
+                    <!-- <li class="hover:text-slate-400"><a href="https://old.sgepequipe.com/relColagem" target="_blank">Relatório de Colagem</a></li> -->
                     <li class="hover:text-slate-400"><a href="/ReservaCliente">Reservas por Cliente</a></li>
                     <!-- <li class="hover:text-slate-400"><a href="https://old.sgepequipe.com/relReservaCLiente" target="_blank">Reservas por Cliente</a></li> -->
                     <li class="hover:text-slate-400"><a href="https://old.sgepequipe.com/relPainelCliente" target="_blank">Painéis por Cliente</a></li>
