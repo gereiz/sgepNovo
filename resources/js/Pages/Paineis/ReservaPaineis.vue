07fe90afa559c7259f904d856c2bcb1b54274e87
georgiereiss <georgie.reis@farmaciaindiana.com.br>
2023-11-16 18:29:34 -0300

    Ajuste de Locate String das reservas

--- resources/js/Pages/Paineis/ReservaPaineis.vue
+++ resources/js/Pages/Paineis/ReservaPaineis.vue
@@ -359,7 +359,7 @@
                             <option value="0" selected disabled>Selecione</option>
                             <option v-for="(bs, index) in listaBisemana"
                                 :key="index" 
-                                :value="bs.id">BS: {{ bs.num_bisemana }} {{ new Date(bs.inicio).toLocaleDateString() }} até {{ new Date(bs.fim).toLocaleDateString() }}
+                                :value="bs.id">BS: {{ bs.num_bisemana }} {{ new Date(bs.inicio).toLocaleDateString('pt-br') }} até {{ new Date(bs.fim).toLocaleDateString('pt-br') }}
                             </option>
                         </select>
                     </div>
