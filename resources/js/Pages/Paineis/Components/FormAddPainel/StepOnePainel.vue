d52e0763744d30f7b43b7f96b49f10646d52d62b
gereiz <georgie.reis@outlook.com>
2024-11-26 14:38:25 -0300

    Correção na adição de painéis

--- resources/js/Pages/Paineis/Components/FormAddPainel/StepOnePainel.vue
+++ resources/js/Pages/Paineis/Components/FormAddPainel/StepOnePainel.vue
@@ -15,7 +15,7 @@ const ufs = ref([]);
 
 onMounted(() => {
     axios.get('/dtGetUf')
-    .then((res) => { 
+    .then((res) => {
         ufs.value = res.data
 
     })
@@ -43,7 +43,7 @@ function getCidades() {
         .catch((err) => {
             console.log(err)
         })
-    
+
 }
 
 function getBairros() {
@@ -58,7 +58,7 @@ function getBairros() {
 
 function emitStep(val) {
     let btnSendOne = document.getElementById('btnSendOne')
-    
+
     btnSendOne.innerHTML = 'Carregando...'
     setTimeout(() => {
         emit('step1', val)
@@ -72,7 +72,7 @@ function toSteptwo() {
 
         toastr.error('O campo Endereço é obrigatório!')
         endereco.focus()
-    
+
     } else if(endPain.value.numero == '') {
         let numero = document.getElementById('numero')
 
@@ -128,7 +128,7 @@ watch(() => props.painel, (val) => {
     endPain.value.bairro = val[0].bairro_id ? val[0].bairro_id : ''
     endPain.value.latitude = val[0].latitude ? val[0].latitude : ''
     endPain.value.longitude = val[0].longitude ? val[0].longitude : ''
-    
+
 })
 
 
@@ -142,7 +142,7 @@ watch(() => props.painel, (val) => {
             <div class="w-full sm:w-6/12">
                 <label for="endereco" class="block text-sm font-medium leading-6 text-gray-900">Endereço</label>
                 <div class="mt-2">
-                    <input type="text" name="endereco" id="endereco" v-model="endPain.ender" :disabled="user.id !==1 && user.id !==1"
+                    <input type="text" name="endereco" id="endereco" v-model="endPain.ender"
                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center" />
                 </div>
             </div>
@@ -150,7 +150,7 @@ watch(() => props.painel, (val) => {
             <div class="w-full sm:w-1/12">
                 <label for="numero" class="block text-sm font-medium leading-6 text-gray-900">Número</label>
                 <div class="mt-2">
-                    <input type="text" name="numero" id="numero" v-model="endPain.numero" :disabled="user.id !==1 && user.id !==1"
+                    <input type="text" name="numero" id="numero" v-model="endPain.numero"
                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center" />
                 </div>
             </div>
@@ -158,7 +158,7 @@ watch(() => props.painel, (val) => {
             <div class="w-full sm:w-3/12">
                 <label for="referencia" class="block text-sm font-medium leading-6 text-gray-900">Ponto de Referência</label>
                 <div class="mt-2">
-                    <input type="text" name="referencia" id="referencia" v-model="endPain.referencia" :disabled="user.id !==1 && user.id !==1"
+                    <input type="text" name="referencia" id="referencia" v-model="endPain.referencia"
                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center" />
                 </div>
             </div>
@@ -170,14 +170,14 @@ watch(() => props.painel, (val) => {
             <div class="w-full sm:w-3/12">
                 <label for="uf" class="block text-sm font-medium leading-6 text-gray-900">UF</label>
                 <div class="mt-2">
-                    <select id="uf" name="uf" v-model="endPain.uf" @change="getCidades()" :disabled="user.id !==1 && user.id !==1"
+                    <select id="uf" name="uf" v-model="endPain.uf" @change="getCidades()"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
                         <option value="0" disabled selected>Selecione</option>
                         <option v-for="(uf, index) in ufs" :key="index" :value="uf.id">{{ uf.nome }}</option>
                     </select>
                 </div>
             </div>
-         
+
             <div class="w-full sm:w-4/12">
                 <label for="cidade" class="block text-sm font-medium leading-6 text-gray-900">Cidade</label>
                 <div class="mt-2">
@@ -187,7 +187,7 @@ watch(() => props.painel, (val) => {
                         <option v-for="(cid, index) in cidades" :key="index" :value="cid.id">{{ cid.nome }}</option>
                     </select>
                 </div>
-            </div> 
+            </div>
 
             <div class="w-full sm:w-3/12">
                 <label for="bairro" class="block text-sm font-medium leading-6 text-gray-900">Bairro</label>
@@ -202,13 +202,13 @@ watch(() => props.painel, (val) => {
 
 
         <div class="w-full flex flex-wrap justify-center space-x-0 sm:space-x-6 space-y-8 sm:space-y-0">
-            
-        
+
+
             <div class="w-full sm:w-4/12">
                 <label for="latitude" class="block text-sm font-medium leading-6 text-gray-900">Latitude</label>
                 <div class="mt-2">
-                    <input type="text" name="latitude" id="latitude" v-model="endPain.latitude" :disabled="user.id !==1 && user.id !==1"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center" 
+                    <input type="text" name="latitude" id="latitude" v-model="endPain.latitude"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center"
                     />
                 </div>
             </div>
@@ -216,8 +216,8 @@ watch(() => props.painel, (val) => {
             <div class="w-full sm:w-4/12">
                 <label for="longitude" class="block text-sm font-medium leading-6 text-gray-900">Longitude</label>
                 <div class="mt-2">
-                    <input type="text" name="longitude" id="longitude" v-model="endPain.longitude" :disabled="user.id !==1 && user.id !==1"
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center"   
+                    <input type="text" name="longitude" id="longitude" v-model="endPain.longitude"
+                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-center"
                     />
                 </div>
             </div>
