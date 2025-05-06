3d526a05a72e982f1ca43d7be6809c746c77e027
gereiz <georgie.reis@outlook.com>
2025-05-06 20:12:58 -0300

    build do commit anterior

--- resources/js/Pages/Financeiro/ComissoesPagas/ComissoesPagas.vue
+++ resources/js/Pages/Financeiro/ComissoesPagas/ComissoesPagas.vue
@@ -113,9 +113,9 @@ function getReservas(idBisemana) {
             </div>
 
             <!-- Cards das PIs -->
-            <div class="card flex flex-col md:flex-row md:flex-wrap w-full h-full bg-base-100 shadow-xl overflow-auto rounded-md p-4 space-y-4 md:space-y-0">
+            <div class="card flex flex-col md:flex-row md:flex-wrap w-full h-full bg-base-100 shadow-xl overflow-auto rounded-md p-4 space-y-4">
                 <div v-for="(pi, index) in pisFiltradas" :key="index" 
-                        class="card bg-neutral text-neutral-content w-96 h-5/6 md:h-4/6 hover:scale-105 transition-all duration-500 ease-in-out md:mx-4 mb-4"
+                        class="card bg-neutral text-neutral-content w-96 h-5/6 md:h-4/6 hover:scale-105 transition-all duration-500 ease-in-out md:mx-4 mt-4"
                 >
                     <div class="card-body items-center text-center">
                         <h2 class="card-title text-2xl">PI nº {{ pi.id }}</h2>
