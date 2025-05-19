3658e5e73acac825efc88e45c15952cd10b91cac
gereiz <georgie.reis@outlook.com>
2025-05-19 12:24:12 -0300

    Ajuste no cadastro de clientes e no cabeçalho do relatório  Painéis x Cliente

---
+++ public/build/assets/toastr-7d941f71.js
@@ -0,0 +1,1 @@
+import{t as o}from"./toastr-85c6e279.js";/* empty css                   */function r(){return o.options.positionClass="toast-top-right",o.options.closeButton=!1,o.options.progressBar=!0,o}export{r as u};
