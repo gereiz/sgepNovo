8967037e6f752a7d7cd64f6484e8202c718c8461
gereiz <georgie.reis@outlook.com>
2026-01-16 18:58:06 -0300

    Criação do botão de cancelar vendas / Ajustes na OS

--- resources/views/relatorios/paineis/rel_disponiveis.blade.php
+++ resources/views/relatorios/paineis/rel_disponiveis.blade.php
@@ -145,7 +145,8 @@
                                     <div style="margin-top: -18%;">
                                         <?php
                                         $filePath = 'storage/'.$p->image_url;
-                                        $originalImage = public_path($filePa                                        $reportImage = $originalImage;
+                                        $originalImage = public_path($filePath);
+                                        $reportImage = $originalImage;
                                         if(file_exists($originalImage) && filesize($originalImage) > 500000){
                                             $info = getimagesize($originalImage);
                                             if ($info['mime'] == 'image/jpeg')
