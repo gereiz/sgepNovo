ff9f7b0b26c6dd91d141fa83143edd494118468b
gereiz <georgie.reis@outlook.com>
2026-02-04 17:45:11 -0300

    Ajuste na exibição das imagens do sistema e relatório 2

--- app/Models/Paineis/Painel.php
+++ app/Models/Paineis/Painel.php
@@ -53,6 +53,9 @@ class Painel extends Model
         if (empty($files)) {
             return $value;
         }
+        $files = array_filter($files, function ($f) {
+            return basename($f) !== 'CompressedJpgImage.jpg';
+        });
         usort($files, function ($a, $b) {
             return Storage::disk('public')->lastModified($b) <=> Storage::disk('public')->lastModified($a);
         });
